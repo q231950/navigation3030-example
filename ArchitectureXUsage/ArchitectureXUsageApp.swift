1e4bb6d1da159345442cacd7d534837cd466addc
Martin Kim Dung-Pham <martinkim.pham@gmail.com>
2021-08-02 19:14:27 +0200

    Initial Commit

---
+++ ArchitectureXUsage/ArchitectureXUsageApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchitectureXUsageApp.swift
+//  ArchitectureXUsage
+//
+//  Created by Martin Kim Dung-Pham on 02.08.21.
+//
+
+import SwiftUI
+
+@main
+struct ArchitectureXUsageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArchitectureXUsage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ArchitectureXUsage
+//
+//  Created by Martin Kim Dung-Pham on 02.08.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
